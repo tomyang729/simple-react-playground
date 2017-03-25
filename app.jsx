20ef372c99f32f64e5a56815e6edb166c213ff13
tomyang729 <tomatoyang729@gmail.com>
2017-03-25 16:22:44 -0700

    first commit

---
+++ app.jsx
@@ -0,0 +1,18 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+class Student extends Component{
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <h1>Hello World!!</h1>
+    );
+  }
+}
+
+export default Student;
---
+++ index.js
@@ -0,0 +1,8 @@
+import Student from './app.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <Student/>,
+  document.getElementById('root')
+)
